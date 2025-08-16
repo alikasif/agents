04004bd6054accf1137b026d021956a4da3c8208
kasif <kasif.ali@gmail.com>
2025-08-16 11:19:05 +0530

    react pattr=ern

--- design_patterns/ReACT/deep_researcher.py
+++ design_patterns/ReACT/deep_researcher.py
@@ -19,8 +19,7 @@ class DeepResearchAgent():
         self.messages.append(SystemMessage(content= self.system_prompt))
 
         self.llm = ChatAnthropic(api_key=os.getenv("ANTHROPIC_API_KEY"), 
-                                 model=os.getenv("ANTHROPIC_MODEL"),
-                                 base_url=os.getenv("ANTHROPIC_BASE_URL"))
+                                 model=os.getenv("ANTHROPIC_MODEL"),)
         self.action_re = re.compile('^Action: (\w+): (.*)$')
         self.google_serper = GoogleSerperAPIWrapper()
         self.available_actions= {"google_search": self._google_search}
@@ -30,8 +29,9 @@ class DeepResearchAgent():
         self.messages.append(HumanMessage(content= user_input))
 
         response = self.llm.invoke(self.messages)
-        self.messages.append(AIMessage(content= response))
-        return response
+        #print(f"\n\nthought response content :: {response.content}\n\n")
+        self.messages.append(AIMessage(content= response.content))
+        return response.content
 
 
     def _parse_result_for_actions(self, result):
@@ -42,21 +42,21 @@ class DeepResearchAgent():
             if action not in self.available_actions:
                 raise Exception("Unknown action: {}: {}".format(action, action_input))
             
-            print(" -- running {} {}".format(action, action_input))
+            print("\n\n-- running {} {}".format(action, action_input))
             action = self.available_actions[action]
             return action, action_input
         else:
-            return
+            return None, None
 
 
     def _google_search(self, query):
-        result = self.google_serper.search(query)
+        result = self.google_serper.run(query)
         return result
 
 
     def _perform_action(self, action, action_input):
         observation = action(action_input)
-        print("Observation:", observation)
+        print("\n\nObservation:", observation)
         next_prompt = "Observation: {}".format(observation)
         return next_prompt
 
@@ -67,15 +67,22 @@ class DeepResearchAgent():
         response=""
         while i < 5:
             response = self._thought(next_prompt)
+            print(f"\n\nthought:: {response}")
             action, action_input = self._parse_result_for_actions(result=response)
-            next_prompt = self._perform_action(action=action, action_input=action_input)
+            if action:
+                print(f"\n\naction = {action} action_input = {action_input}")
+                next_prompt = self._perform_action(action=action, action_input=action_input)
+                print(f"\n\nnext prompt {next_prompt}")
+            else:
+                print(f"*** NO ACTION FOUND!!")
+            i+=1
         return response
 
 
 if __name__ == '__main__':
 
     load_dotenv(override=True)
-    user_input = input("Enter your stock research query: ")
+    user_input = input("Enter your research query: ")
     agent = DeepResearchAgent(stock_research_prompt)
     response = agent.run(user_input=user_input)
     print(f"\n\n final response: \n\n {response}")
